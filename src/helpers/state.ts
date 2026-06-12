97a0aa3fad842e9bf54f9653dec9fe256c79f4fb
Oussama Saadaoui <oussama.saadaoui.dev@gmail.com>
2026-06-12 04:31:09 +0200

    fix: allow optional slice parameter in normalizeState function

--- src/helpers/state.ts
+++ src/helpers/state.ts
@@ -1,10 +1,10 @@
 export const nestingSeparator = ".";
 
-export function normalizeState(state: any, slice: string) {
+export function normalizeState(state: any, slice?: string) {
 	const normalizedState = { children: {} } as any;
 
 	const slices = Object.keys(state)
-		.filter((key) => key.startsWith(slice))
+		.filter((key) => key.startsWith(slice || ""))
 		.map((key) => key.split(nestingSeparator))
 		.sort((a, b) => a.length - b.length);
 
